84fa4364b122ea40aa128596ec15441238c331f6
PedroPinho23 <pedrohpinho2@gmail.com>
2024-10-10 05:00:33 -0300

    Add files via upload

---
+++ pagina1/ordenando.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const swap = (array, i, j) => {
+    [array[i], array[j]] = [array[j], array[i]];
+};
+
+const shuffle = (array, quant = array.length) => {
+    for (let i = 0; i < quant; i++) {
+        const pos1 = Math.floor(Math.random() * array.length);
+        const pos2 = Math.floor(Math.random() * array.length);
+        swap(array, pos1, pos2);
+    }
+};
+
+const bubbleSort = (array) => {
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = 0; j < array.length - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                swap(array, j, j + 1);
+            }
+        }
+    }
+};
+
+const selectionSort = (array) => {
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] > array[j]) {
+                swap(array, i, j);
+            }
+        }
+    }
+};
+
+const quickSort = (array, start = 0, end = array.length - 1) => {
+    start = start == undefined ? 0 : start;
+    end = end == undefined ? array.length - 1 : end;
+    if (start >= end) {
+        return;
+    }
+    const index = partition(array, start, end);
+    quickSort(array, start, index - 1);
+    quickSort(array, index + 1, end);
+};
+
+const partition = (array, start, end) => {
+    let pivotIndex = start;
+    for (let i = start; i < end; i++) {
+        if (array[i] < array[end]) {
+            swap(array, i, pivotIndex);
+            pivotIndex++;
+        }
+    }
+    swap(array, pivotIndex, end);
+    return pivotIndex;
+};
+
+const limparValor = () => {
+    document.getElementById('valor').value = '';
+};
+
+const add = () => {
+    const valor = Number(document.getElementById('valor').value);
+    const textNode = document.createTextNode(valor);
+    const node = document.createElement('li');
+    node.appendChild(textNode);
+    document.getElementById('valores').appendChild(node);
+    limparValor();
+};
+
+const limparValores = () => {
+    limparValor();
+    document.getElementById('valores').innerHTML = '';
+};
+
+const ordenar = () => {
+    const lista = document.getElementById('valores');
+    const selects = document.getElementById('selects');
+    const array = Array.from(lista.children).map(item => parseInt(item.innerHTML));
+    const algoritmo = selects.options[selects.selectedIndex].value;
+    switch (algoritmo) {
+        case 'bubbleSort': bubbleSort(array); break;
+        case 'selectionSort': selectionSort(array); break;
+        case 'quickSort': quickSort(array, 0, array.length - 1); break;
+    }
+    lista.innerHTML = array
+        .map(item => `<li>${item}</li>`)
+        .reduce((acumulador, item) => acumulador + item, '');
+};
+
+const misturar = () => {
+    const lista = document.getElementById('valores');
+    const array = Array.from(lista.children);
+    shuffle(array);
+    for (let i = 0; i < array.length; i++) {
+        lista.appendChild(array[i]);
+    }
+};
+
+const input = document.getElementById('valor');
+input.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        document.getElementById('addElement').click();
+    }
+});
---
+++ pagina2/receitas.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const receitas = [
+    {
+        titulo: 'Arroz de Couve-Flor',
+        imagem: '../img/arroz.png',
+        preparo: 'Deixe a couve-flor picada, adicione os ingredientes e refogue ' +
+            'bem, adicione sal, tampe a panela e deixe cozinhar.',
+        ingredientes: ['Arroz', 'Couve-Flor', 'Cebola Media', 'Azeite', 'Sal'],
+    },
+    {
+        titulo: 'Bolo de Café Cremoso',
+        imagem: '../img/bolo.png',
+        preparo: 'Bata o açúcar, as gemas e o café, adicione farinha e chocolate ' +
+            'e mexa bem, bata as claras e junte a mistura.',
+        ingredientes: ['Farinha de Trigo', 'Açúcar', 'Café Coado',
+            'Chocolate em Pó', 'Ovos'],
+    },
+    {
+        titulo: 'Coxinha de Brigadeiro',
+        imagem: '../img/coxinha-doce.png',
+        preparo: 'Junte o leite condensado, chocolate em pó e manteiga, aqueça ' +
+            'no fogo baixo, envolva os morangos e passe no granulado.',
+        ingredientes: ['Leite Condensado', 'Chocolate em Pó', 'Manteiga',
+            'Morango', 'Chocolate Granulado'],
+    }
+];
+
+const getListaIngredientes = (receita) => receita.ingredientes
+    .map(ingrediente => `<li class="mb-1">${ingrediente}</li>`)
+    .reduce((acumulador, item) => acumulador + item, '<ul class="mb-4">') + '</ul>';
+
+
+const getCard = (receita) =>
+    `<div class='card rounded-5 mx-2' style='width: 320px; height: 600px;'> 
+     <img src='${receita.imagem}' class='card-img-top bg-warning'>
+     <div class='card-body'>
+       <h5 class='card-title text-center fs-4 p-2 fw-bold'>${receita.titulo}</h5>
+       <div class='card-text'>${getListaIngredientes(receita)}</div>
+       <hr>
+       <p class='card-text p-2'>${receita.preparo}</p>
+     </div>
+   </div>`;
+
+const preencheCatalogo = () => {
+    document.getElementById('pnlCatalogo').innerHTML = receitas
+        .map(receita => getCard(receita))
+        .reduce((acumulador, item) => acumulador + item, '');
+}
+
+document.onload = preencheCatalogo();
